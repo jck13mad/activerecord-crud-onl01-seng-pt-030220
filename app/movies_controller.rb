6f9ff7b24836f99871a6fdf74a3f3899c746e8f9
Jack Madden <jck13mad@gmail.com>
2020-05-28 22:19:52 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -77,7 +77,6 @@ def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
   movie = Movie.update_all(title: "Wat, huh?")
-  __
 end
 
 def can_update_multiple_items_at_once
