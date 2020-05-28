642eab37c6d1ab7997c1fcad4d01e2d7cfb4dae5
Jack Madden <jck13mad@gmail.com>
2020-05-28 22:46:07 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,10 +24,9 @@ end
 
 def can_be_created_in_a_block
   Movie.create do |m|
-    m.title = "Movie_0"
+    m.title = "Home Alone"
     m.release_date = 1990
   end
-  m.save
 end
 
 def can_get_the_first_item_in_the_database
